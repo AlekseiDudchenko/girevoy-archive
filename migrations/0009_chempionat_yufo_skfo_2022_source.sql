b92d316f6ae10f93e3be492d142b0afad3eca497
Aleksei Dudchenko <alexey.Dudchenko@gmail.com>
2026-09-17 23:49:29 +0200

    Добавить данные чемпионата ЮФО и СКФО 2022

---
+++ migrations/0009_chempionat_yufo_skfo_2022_source.sql
@@ -0,0 +1,5 @@
+-- Source mapping for the 2022 Southern and North Caucasian Federal District championships.
+INSERT INTO competition_source_urls (competition_slug, source_url, archive_path) VALUES
+  ('chempionat-yufo-skfo-2022',
+   'https://vfgs.ru/assets/files/sorevnovania/2022/chempionat-yufo-i-skfo.xlsx',
+   'sources/2022/chempionat-yufo-i-skfo.xlsx');
