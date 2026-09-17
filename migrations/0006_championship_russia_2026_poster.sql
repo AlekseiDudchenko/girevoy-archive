8554e5ea2be153655d9e1a5080c86043c81dc6cd
Aleksei Dudchenko <alexey.Dudchenko@gmail.com>
2026-09-17 20:30:00 +0200

    Assign poster to Championship of Russia 2026

---
+++ migrations/0006_championship_russia_2026_poster.sql
@@ -0,0 +1,35 @@
+-- Poster assignment for Championship of Russia 2026.
+-- The poster asset is stored locally under public/competition-posters.
+
+CREATE TABLE IF NOT EXISTS competition_posters (
+  competition_slug TEXT PRIMARY KEY,
+  poster_path      TEXT NOT NULL
+);
+
+INSERT OR REPLACE INTO competition_posters (competition_slug, poster_path) VALUES
+  ('chempionat-rossii-2026', '/competition-posters/chempionat-rossii-2026.jpg');
+
+UPDATE competitions
+SET poster_path = (
+  SELECT poster_path
+  FROM competition_posters
+  WHERE competition_slug = competitions.slug
+)
+WHERE slug = 'chempionat-rossii-2026';
+
+CREATE TRIGGER IF NOT EXISTS competitions_fill_poster_after_insert
+AFTER INSERT ON competitions
+BEGIN
+  UPDATE competitions
+  SET poster_path = (
+    SELECT poster_path
+    FROM competition_posters
+    WHERE competition_slug = NEW.slug
+  )
+  WHERE id = NEW.id
+    AND EXISTS (
+      SELECT 1
+      FROM competition_posters
+      WHERE competition_slug = NEW.slug
+    );
+END;
