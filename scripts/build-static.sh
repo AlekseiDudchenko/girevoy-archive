059400dc34b9e117c18c51d696cab9d31d054d2f
Aleksei Dudchenko <alexey.Dudchenko@gmail.com>
2026-09-17 23:50:24 +0200

    Подключить чемпионат ЮФО и СКФО 2022 к сборке

--- scripts/build-static.sh
+++ scripts/build-static.sh
@@ -34,6 +34,7 @@ python3 scripts/gen_seed.py data/chempionat-yufo-skfo-2026.json > "$BUILD_DIR/ch
 python3 scripts/gen_seed.py data/chempionat-yufo-skfo-2025.json > "$BUILD_DIR/chempionat-yufo-skfo-2025.sql"
 python3 scripts/gen_seed.py data/chempionat-yufo-skfo-2024.json > "$BUILD_DIR/chempionat-yufo-skfo-2024.sql"
 python3 scripts/gen_seed.py data/chempionat-yufo-skfo-2023.json > "$BUILD_DIR/chempionat-yufo-skfo-2023.sql"
+python3 scripts/gen_seed.py data/chempionat-yufo-skfo-2022.json > "$BUILD_DIR/chempionat-yufo-skfo-2022.sql"
 python3 scripts/gen_seed.py data/chempionat-tsfo-2026.json > "$BUILD_DIR/chempionat-tsfo-2026.sql"
 python3 scripts/gen_seed.py data/chempionat-tsfo-2025.json > "$BUILD_DIR/chempionat-tsfo-2025.sql"
 python3 scripts/gen_seed.py data/chempionat-tsfo-2024.json > "$BUILD_DIR/chempionat-tsfo-2024.sql"
@@ -71,6 +72,7 @@ cat \
   "$BUILD_DIR/chempionat-yufo-skfo-2025.sql" \
   "$BUILD_DIR/chempionat-yufo-skfo-2024.sql" \
   "$BUILD_DIR/chempionat-yufo-skfo-2023.sql" \
+  "$BUILD_DIR/chempionat-yufo-skfo-2022.sql" \
   "$BUILD_DIR/chempionat-tsfo-2026.sql" \
   "$BUILD_DIR/chempionat-tsfo-2025.sql" \
   "$BUILD_DIR/chempionat-tsfo-2024.sql" \
