87e0308aa9b6b16c95afd618bb960ac59c30194f
Aleksei Dudchenko <alexey.Dudchenko@gmail.com>
2026-09-17 19:44:05 +0200

    Move tournament images to separate PR

--- migrations/0005_competition_images.sql
+++
@@ -1,5 +0,0 @@
--- Optional visual identity for competition cards.
--- Posters are preferred over logos in the public UI when both are available.
-
-ALTER TABLE competitions ADD COLUMN poster_url TEXT;
-ALTER TABLE competitions ADD COLUMN logo_url TEXT;
