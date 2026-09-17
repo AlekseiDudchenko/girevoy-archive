abf095f302f01d4cada974d5cf17617a2703da41
Aleksei Dudchenko <alexey.Dudchenko@gmail.com>
2026-09-17 22:56:50 +0200

    Добавить данные чемпионата СЗФО 2022

---
+++ migrations/0007_chempionat_szfo_2022_source.sql
@@ -0,0 +1,5 @@
+-- Source mapping for the 2022 Northwestern Federal District championship.
+INSERT INTO competition_source_urls (competition_slug, source_url, archive_path) VALUES
+  ('chempionat-szfo-2022',
+   'https://vfgs.ru/assets/files/sorevnovania/2022/protokol-chemfoszfo-2022.xlsx',
+   'sources/2022/protokol-chemfoszfo-2022.xlsx');
