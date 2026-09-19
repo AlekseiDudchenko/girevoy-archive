4dfeb25879b39eb752d0959c493250390ffc8d9a
Aleksei Dudchenko <alexey.Dudchenko@gmail.com>
2026-09-19 14:32:36 +0200

    Remove Russia Cup 2026 poster assignment

--- migrations/0014_kubok_rossii_2026_poster.sql
+++
@@ -1,5 +0,0 @@
--- Poster assignment for Russia Cup 2026.
--- The poster image is stored under public/competition-posters.
-
-INSERT OR REPLACE INTO competition_posters (competition_slug, poster_path) VALUES
-  ('kubok-rossii-2026', '/competition-posters/kubok-rossii-2026.jpg');
